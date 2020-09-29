16abf6ddab3949d5c66c06ba81fad4a51cdc5c18
Paulo Farah <paulorfarah@gmail.com>
2020-09-28 23:30:52 -0300

    count valley added constraints and problem definition

--- hackerrank/countvalleys/main.go
+++ hackerrank/countvalleys/main.go
@@ -2,29 +2,69 @@ package main
 
 import "fmt"
 
+/*
+Counting Valleys
+
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+Example
+
+The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+
+Function Description
+
+Complete the countingValleys function in the editor below.
+
+countingValleys has the following parameter(s):
+
+int steps: the number of steps on the hike
+string path: a string describing the path
+Returns
+
+int: the number of valleys traversed
+Input Format
+
+The first line contains an integer , the number of steps in the hike.
+The second line contains a single string , of  characters that describe the path.
+*/
+
 func countValleys(steps int32, path string) int32 {
 	count := 0
 	var valleys int32 
 	typeH := 's'
-	for _, c := range path {
-	    if c == 'D' {
-		    count--
-	    } else {
-		    count ++
-	    }
-	    if typeH == 's' {
-		    if count < 0 {
-			    typeH = 'v'
-		    }else if count > 0 {
-			    typeH = 'm'
+
+	//check constraints
+	if len(path) < 2 || len(path) > 1000000{
+		return 0
+	}
+
+	if steps >= 2 && steps <= 1000000 {
+		for _, c := range path {
+		    if c == 'D' {
+			    count--
+		    } else if c == 'U' {
+			    count ++
+		    } else {
+			    return 0
 		    }
-	    }else if count == 0 {
-		    if typeH == 'v' {
-			    valleys++
+		    if typeH == 's' {
+			    if count < 0 {
+				    typeH = 'v'
+			    }else if count > 0 {
+				    typeH = 'm'
+			    }
+		    }else if count == 0 {
+			    if typeH == 'v' {
+				    valleys++
+			    }
+			    typeH = 's'
 		    }
-		    typeH = 's'
-	    }
-    	}
+    		}
+	}
 	return valleys
 }
 
@@ -51,6 +91,11 @@ func main() {
 	//test case 5
 	path = "UDDUUDDU"
 	v = countValleys(8, path)
+	fmt.Println(v)
+
+	//test case 6
+	path = "U"
+	v = countValleys(1, path)
 	fmt.Println(v)
 
 }
