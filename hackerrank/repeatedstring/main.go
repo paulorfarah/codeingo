77c12c4a613649966b857135f03ad977aa028c83
Paulo Farah <paulorfarah@gmail.com>
2020-10-03 09:31:23 -0300

    refactored var declaration and removed printing commands

--- hackerrank/repeatedstring/main.go
+++ hackerrank/repeatedstring/main.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func repeatedString(s string, n int64) int64 {
-	var count int64
-	var i int64
-	var subTimes int64 = n/int64(len(s))
-	var remainder int64 = int64(n % int64(len(s)))
+	var (
+		count int64
+		i int64
+		subTimes int64 = n/int64(len(s))
+		remainder int64 = int64(n % int64(len(s)))
+	)
 
-	fmt.Println(remainder)
 	for _, c := range s {
 		if c == 'a' {
 			count++
 		}
 	}
 	count *= subTimes
-	fmt.Println(count)
 
 	for i = 0; i < remainder; i++ {
 		if s[i] == 'a' {
