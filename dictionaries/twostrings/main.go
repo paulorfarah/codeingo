e48e232246c1208f954ec721cf5d495e89143b82
Paulo Farah <paulorfarah@gmail.com>
2020-12-14 11:24:23 -0300

    optimized version of intersection of two strings

--- dictionaries/twostrings/main.go
+++ dictionaries/twostrings/main.go
@@ -9,21 +9,16 @@ func main(){
 	w2 := "world"
 
 	d1 := make(map[string]bool)
-	r1 := []rune(w1)
-	r2 := []rune(w2)
-	for i := 0; i <= len(w1); i++ {
-		for j := i+1; j <= len(w1); j++ {
-			fmt.Println(string(r1[i:j]))
-			d1[string(r1[i:j])] = true
-		}
+	for _, i := range(w1){ 
+		fmt.Println(i)
+		d1[string(i)] = true
 	}
-	for i := 0; i <= len(w2); i++ {
-		for j := i+1; j <= len(w2); j++ {
-			_, found := d1[string(r2[i:j])]
-			if found {
-				//return "YES"
-				fmt.Println("YES")
-			}
+	for _, i := range w2{
+		_, found := d1[string(i)]
+		if found {
+			//return "YES"
+			fmt.Println("YES")
+			return
 		}
 	}
 //	return "NO"
