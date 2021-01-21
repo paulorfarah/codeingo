ea87f48f0fe451e27415521ee4469f7a2225afbc
Paulo Farah <paulorfarah@gmail.com>
2021-01-21 14:17:21 -0300

    harckerhank sherlock and anagrams solved

--- hackerrank/dictionaries/sherlockandanagrams/main.go
+++ hackerrank/dictionaries/sherlockandanagrams/main.go
@@ -1,44 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
-func main() {
-	s := "cdcd"
-	var list []string
+
+func SherlockAndAnagrams(s string) int {
+	hashMap := make(map[string]int)
 	r := []rune(s)
 	for i := 0; i < len(s); i++ {
-		for j := i + 1; j <= len(s); j++ {
-			list = append(list, string(r[i:j]))
-		}
-	}
-
-	q := 0
-	for i := 0; i < len(list) - 1; i++ {
-		r1 := []rune(list[i])
-		for k := i + 1; k < len(list); k++ {
-			m := make(map[string]int)
-			for j:= 0; j < len(r1); j++ {
-				m[string(r1[j])]++
-			}
-		//	fmt.Println(m)
-			r2 := []rune(list[k])
-			fmt.Println(string(r1), string(r2))
-			for j := 0; j < len(r2); j++ {
-				m[string(r2[j])]--
-			}
-			//fmt.Println(m)
-
-			equal := true
-			for _, v := range m {
-				if v != 0 {
-					equal = false
-				}
-			}
-			if equal {
-				q++
+		for j := i; j < len(s); j++ {
+			c := strings.Split(string(r[i:j+1]), "")
+			sort.Strings(c)
+			k := strings.Join(c, "")
+			if _, ok := hashMap[k]; ok {
+				hashMap[k] += 1
+			} else {
+				hashMap[k] = 1
 			}
 		}
 	}
+	pairs := 0;
+	for _, v := range hashMap {
+		pairs += (v * (v - 1)) / 2
+	}
+	return pairs
+}
+
+
+
+func main() {
+	s := ""
+	q := SherlockAndAnagrams(s)
 	fmt.Println(q)
 
 }
